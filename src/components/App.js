1bd2c5f3b939a1d4b0ddc1a15a0255a849179c4b
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-11 14:03:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
 import React from "react";
 import './../styles/App.css';
+import Todo from './Todo';
+
 
 const App = () => {
+const [tasks,setTasks] = useState([]);
+
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className='app'>
+      <Todo setTasks={setTasks} />
+      {
+        tasks?.map((item, index) => {
+          return <div key={index}>
+            <p>{item}</p>
+            <button className='delete-btn' onClick={()=>deleteTask(item.id)}>Delete</button>
+          </div>
+        })
+      }
     </div>
   )
 }
